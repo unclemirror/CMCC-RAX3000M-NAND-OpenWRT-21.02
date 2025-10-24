d8ed43fa13f988d132ed5927ffd766c4b4158589
unclemirror <unclemirror526@gmail.com>
2025-10-24 09:16:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,17 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+##-----------------Replace latest OpenClash------------------
+git clone --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv /tmp/OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
+rm -rf /tmp/OpenClash
+
+##-----------------Add latest OpenClash meta core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
